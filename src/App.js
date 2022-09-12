48339ec7aea1e159e0275487c44811874194c82e
Sanglim Lee <82.limii@gmail.com>
2022-09-13 00:01:43 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          안녕하세요.
+          react project test
         </p>
         <a
           className="App-link"
